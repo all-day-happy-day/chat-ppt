62f376f328f48589b300ceeaea65aabdfdde0e3c
mand-ollar <minsec9912@icloud.com>
2026-03-22 01:02:49 +0900

    feat: DATETIME is not available for postgres, change it to Datetime

--- api/app/user/infrastructure/repository/entity/UserAlchemyEntity.py
+++ api/app/user/infrastructure/repository/entity/UserAlchemyEntity.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timezone
 
-from sqlalchemy import Enum, String
-from sqlalchemy.dialects.mysql import DATETIME
+from sqlalchemy import DateTime, Enum, String
 from sqlalchemy.orm import Mapped, mapped_column
 from ulid import ULID
 
@@ -17,10 +16,10 @@ class UserAlchemyEntity(Base):
     password: Mapped[str] = mapped_column(String(64), nullable=False)
     email: Mapped[str] = mapped_column(String(32), unique=True, nullable=False)
     created_at: Mapped[datetime] = mapped_column(
-        DATETIME(fsp=6), nullable=False, default=lambda: datetime.now(tz=timezone.utc)
+        DateTime(timezone=True), nullable=False, default=lambda: datetime.now(tz=timezone.utc)
     )
     updated_at: Mapped[datetime] = mapped_column(
-        DATETIME(fsp=6), nullable=False, default=lambda: datetime.now(tz=timezone.utc)
+        DateTime(timezone=True), nullable=False, default=lambda: datetime.now(tz=timezone.utc)
     )
-    last_sign_in: Mapped[datetime] = mapped_column(DATETIME(fsp=6), nullable=True)
+    last_sign_in: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=True)
     role: Mapped[Role] = mapped_column(Enum(Role), nullable=False, default=Role.USER)
