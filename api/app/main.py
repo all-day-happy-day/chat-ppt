c353ceae7e7b494065726ab9c30fd76de3d0d899
mand-ollar <minsec9912@icloud.com>
2026-03-21 19:31:31 +0900

    feat: add app.main

---
+++ api/app/main.py
@@ -0,0 +1,43 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from pydantic_core import ValidationError as CoreValidationError
+
+from app.user.infrastructure.adapter.inbound.api import router as user_api_router
+
+app: FastAPI = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:8000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(user_api_router, prefix="/user")
+
+
+@app.exception_handler(RequestValidationError)
+async def handle_request_validation_error(request, exc: RequestValidationError) -> JSONResponse:
+    return JSONResponse(
+        status_code=400,
+        content=exc.errors(),
+    )
+
+
+@app.exception_handler(CoreValidationError)
+async def handle_core_validation_error(request, exc: CoreValidationError):
+    return JSONResponse(
+        status_code=422,
+        content=jsonable_encoder({"detail": exc.errors()}),
+    )
+
+
+if __name__ == "__main__":
+    uvicorn.run(app=app, host="127.0.0.1", port=8000, reload=True)
