aa460383c3e7c8f9cd8fe9c7e69440c373a480ee
mand-ollar <minsec9912@icloud.com>
2026-03-25 18:36:41 +0900

    feat: add username validator logic; not contain "@"

--- api/app/user/domain/entity/User.py
+++ api/app/user/domain/entity/User.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, model_validator
 from ulid import ULID
 
 from app.user.domain.enum import Role
@@ -15,3 +15,9 @@ class User(BaseModel):
     updated_at: datetime
     last_sign_in: datetime | None = Field(default=None)
     role: Role = Field(default=Role.USER)
+
+    @model_validator(mode="after")
+    def validate_username(self) -> "User":
+        if "@" in self.username:
+            raise ValueError("Invalid username. Username cannot contain '@'.")
+        return self
