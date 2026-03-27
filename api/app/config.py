628e56c117385ea26bd73a1d07534751fda8c445
mand-ollar <minsec9912@icloud.com>
2026-03-27 13:35:30 +0900

    feat: add adhd email account to .env

--- api/app/config.py
+++ api/app/config.py
@@ -1,8 +1,14 @@
+from pydantic import EmailStr, Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Config(BaseSettings):
-    auth_secret_key: str
-    auth_token_life_day: int
+    auth_secret_key: str = Field(alias="AUTH_SECRET_KEY")
+    auth_token_life_day: int = Field(alias="AUTH_TOKEN_LIFE_DAY")
+    email_sender_email: EmailStr = Field(alias="EMAIL_SENDER_EMAIL")
+    email_sender_password: str = Field(alias="EMAIL_SENDER_PASSWORD")
 
     model_config = SettingsConfigDict(env_file=".env")
+
+
+config: Config = Config()  # type: ignore[call-arg]
