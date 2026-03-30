e6d078714b76de5d28223b12955f676134fbb062
mand-ollar <minsec9912@icloud.com>
2026-03-31 07:02:07 +0900

    feat: implement AlchemyLyricsRepository

--- api/app/song/domain/exception/__init__.py
+++ api/app/song/domain/exception/__init__.py
@@ -1,7 +1,9 @@
 from .FailedToFetch import FailedToFetch
+from .LyricsNotFound import LyricsNotFound
 from .SongNotFound import SongNotFound
 
 __all__ = [
     "FailedToFetch",
+    "LyricsNotFound",
     "SongNotFound",
 ]
--- api/app/song/infrastructure/respository/lyrics/AlchemyLyricsRepository.py
+++ api/app/song/infrastructure/respository/lyrics/AlchemyLyricsRepository.py
@@ -1,8 +1,36 @@
 from sqlalchemy.orm import Session
+from ulid import ULID
 
+from app.song.domain.exception import LyricsNotFound
 from app.song.domain.repository import LyricsRepository
+from app.song.domain.valueobject import Lyrics
+from app.song.infrastructure.respository.lyrics.entity import LyricsAlchemyEntity
+from app.song.infrastructure.respository.lyrics.mapper import LyricsMapper
 
 
 class AlchemyLyricsRepository(LyricsRepository):
     def __init__(self, db: Session) -> None:
         self.db: Session = db
+
+    def save(self, lyrics: Lyrics) -> Lyrics:
+        alchemy_entity: LyricsAlchemyEntity = LyricsMapper.to_alchemy_entity(lyrics)
+        self.db.merge(alchemy_entity)
+        self.db.commit()
+        return lyrics
+
+    def get_by_song_id(self, song_id: ULID) -> Lyrics:
+        alchemy_entity: LyricsAlchemyEntity | None = (
+            self.db.query(LyricsAlchemyEntity).filter(LyricsAlchemyEntity.song_id == str(song_id)).first()
+        )
+        if alchemy_entity is None:
+            raise LyricsNotFound(f"Lyrics not found: {song_id}")
+        return LyricsMapper.to_domain_entity(alchemy_entity)
+
+    def delete_by_song_id(self, song_id: ULID) -> None:
+        alchemy_entity: LyricsAlchemyEntity | None = (
+            self.db.query(LyricsAlchemyEntity).filter(LyricsAlchemyEntity.song_id == str(song_id)).first()
+        )
+        if alchemy_entity is None:
+            raise LyricsNotFound(f"Lyrics not found: {song_id}")
+        self.db.delete(alchemy_entity)
+        self.db.commit()
