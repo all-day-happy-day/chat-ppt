0c7b10bbd260e48683fc6117c305bed6934a4025
mand-ollar <minsec9912@icloud.com>
2026-04-08 08:49:45 +0900

    feat: remove user_id field from ProjectContainer

--- api/app/project/domain/entity/ProjectContainer.py
+++ api/app/project/domain/entity/ProjectContainer.py
@@ -9,7 +9,6 @@ from .PartsMixin import PartsMixin
 class ProjectContainer(PartsMixin):
     id: ULID = Field(default_factory=lambda: ULID())
     project_id: ULID
-    user_id: ULID
     container_name: str
     created_at: datetime
     updated_at: datetime
--- api/app/project/infrastructure/repository/project_container/entity/ProjectContainerAlchemyEntity.py
+++ api/app/project/infrastructure/repository/project_container/entity/ProjectContainerAlchemyEntity.py
@@ -13,7 +13,6 @@ class ProjectContainerAlchemyEntity(Base):
 
     id: Mapped[str] = mapped_column(String(36), primary_key=True, unique=True, default=lambda: str(ULID()))
     project_id: Mapped[str] = mapped_column(ForeignKey("project.id", ondelete="CASCADE"), nullable=False, index=True)
-    user_id: Mapped[str] = mapped_column(String(36), nullable=False, index=True)
     container_name: Mapped[str] = mapped_column(String(64), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
