233d5b32c82be03ebaa5101075f42ae0d9f07c70
mand-ollar <minsec9912@icloud.com>
2026-04-13 17:56:02 +0900

    chore

--- api/app/user/infrastructure/adapter/inbound/api/message/BaseUserResponse.py
+++ api/app/user/infrastructure/adapter/inbound/api/message/BaseUserResponse.py
@@ -3,12 +3,20 @@ from typing import Self
 from pydantic import BaseModel
 
 from app.user.domain.entity import User
+from app.user.domain.enum import Role
 
 
 class BaseUserResponse(BaseModel):
     id: str
     username: str
+    email: str
+    role: Role
 
     @classmethod
     def from_domain_entity(cls, user: User) -> Self:
-        return cls(id=str(user.id), username=user.username)
+        return cls(
+            id=str(user.id),
+            username=user.username,
+            email=str(user.email),
+            role=user.role,
+        )
