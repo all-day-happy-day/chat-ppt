592c7bb2a50d4de7fdffee78ad5415ff5b33ecae
mand-ollar <minsec9912@icloud.com>
2026-04-15 08:52:01 +0900

    feat: add more data for layout response

--- api/app/powerpoint/infrastructure/adapter/inbound/api/message/GetLayoutResponse.py
+++ api/app/powerpoint/infrastructure/adapter/inbound/api/message/GetLayoutResponse.py
@@ -3,14 +3,20 @@ from typing import Self
 from pydantic import BaseModel
 
 from app.powerpoint.domain.entity import Layout, Shape
-from app.powerpoint.domain.valueobject import Size
+from app.powerpoint.domain.valueobject import ColorConfig, Size
 
 
 class GetLayoutResponse(BaseModel):
     name: str
     shapes: list[Shape]
     slide_size: Size
+    background_color: ColorConfig
 
     @classmethod
     def from_domain_entity(cls, layout: Layout, slide_size: Size) -> Self:
-        return cls(name=layout.name, shapes=layout.shapes, slide_size=slide_size)
+        return cls(
+            name=layout.name,
+            shapes=layout.shapes,
+            slide_size=slide_size,
+            background_color=layout.background_color,
+        )
