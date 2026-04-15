483a1624cc2326ff82ff3ebc6f6318647d11a62b
mand-ollar <minsec9912@icloud.com>
2026-04-15 14:46:12 +0900

    feat: add invalid part index exception

--- api/app/project/application/usecase/InsertPartUseCase.py
+++ api/app/project/application/usecase/InsertPartUseCase.py
@@ -1,6 +1,7 @@
 from ulid import ULID
 
 from app.project.domain.entity import Project
+from app.project.domain.exception import InvalidPartIndex
 from app.project.domain.repository import ProjectRepository
 from app.project.domain.types import Parts
 
@@ -11,5 +12,7 @@ class InsertPartUseCase:
 
     def __call__(self, project_id: ULID, part: Parts, index: int) -> Project:
         project: Project = self.project_repository.get_by_id(id=project_id)
+        if index < 0 or index > len(project.parts):
+            raise InvalidPartIndex(f"Invalid part index: {index}")
         project.parts.insert(index, part)
         return self.project_repository.save(project=project)
