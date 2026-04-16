a91d422f3018247739f241b8067425c5ad362990
Minseok Oh <minsec9912@icloud.com>
2026-04-16 21:35:43 +0900

    fix: resolve circular import

---
+++ api/app/bible/domain/service/ParseVerseRangeService.py
@@ -0,0 +1,34 @@
+import re
+
+from app.bible.domain.exception import MultipleNumbers, MultipleSeparators, UnsupportedLetter
+
+
+class ParseVerseRangeService:
+    SEP: list[str] = ["-", "~", ","]
+
+    def parse(self, verse: str) -> list[int]:
+        # Find unsupported letters
+        survived: list[str] = re.compile(f"[0-9{''.join(self.SEP)}]+").findall(verse)
+        if len(survived) != len(verse):
+            raise UnsupportedLetter(f"Unsupported letter: {verse}")
+
+        # Find multiple separators
+        seps: list[str] = re.compile(f"[{''.join(self.SEP)}]").findall(verse)
+        if len(seps) > 1:
+            raise MultipleSeparators(f"Multiple separators: {verse}")
+
+        # Parse
+        numbers: list[int] = re.compile(r"[0-9]+").findall(verse)
+        if seps:
+            if seps[0] in self.SEP[0:2]:  # range
+                if len(numbers) != 2:
+                    raise MultipleNumbers(f"Multiple numbers: {verse}")
+                st: int = int(numbers[0])
+                en: int = int(numbers[1])
+                return list(range(st, en + 1))
+            elif seps[0] == self.SEP[2]:  # comma
+                return [int(number) for number in numbers]
+            else:
+                raise NotImplementedError
+        else:
+            return [int(number) for number in numbers]
--- api/app/bible/domain/service/__init__.py
+++ api/app/bible/domain/service/__init__.py
@@ -1,5 +1,7 @@
 from .ParseVerseQueryService import ParseVerseQueryService
+from .ParseVerseRangeService import ParseVerseRangeService
 
 __all__ = [
     "ParseVerseQueryService",
+    "ParseVerseRangeService",
 ]
