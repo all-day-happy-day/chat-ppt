20880592113fe6236843904c7480b304358b5035
Minseok Oh <minsec9912@icloud.com>
2026-04-16 23:38:25 +0900

    chore: abstractmethod pass -> raise NotImplementError

--- api/app/user/domain/repository/UserRepository.py
+++ api/app/user/domain/repository/UserRepository.py
@@ -9,28 +9,28 @@ from app.user.domain.entity import User
 class UserRepository(ABC):
     @abstractmethod
     def save(self, user: User) -> User:
-        pass
+        raise NotImplementedError
 
     @abstractmethod
     def patch(self, user: User) -> User:
-        pass
+        raise NotImplementedError
 
     @abstractmethod
     def delete_by_id(self, id: ULID) -> None:
-        pass
+        raise NotImplementedError
 
     @abstractmethod
     def get_by_id(self, user_id: ULID) -> User:
-        pass
+        raise NotImplementedError
 
     @abstractmethod
     def get(self) -> list[User]:
-        pass
+        raise NotImplementedError
 
     @abstractmethod
     def get_by_email(self, email: EmailStr) -> User:
-        pass
+        raise NotImplementedError
 
     @abstractmethod
     def get_by_username(self, username: str) -> User:
-        pass
+        raise NotImplementedError
