0367d00d520eefd5cf59b2f5dfc0054f044093fe
Minseok Oh <minsec9912@icloud.com>
2026-04-16 22:59:25 +0900

    feat: change get_verses api return; from list to dict

--- api/app/bible/infrastructure/adapter/inbound/api/message/GetVersionsResponse.py
+++ api/app/bible/infrastructure/adapter/inbound/api/message/GetVersionsResponse.py
@@ -1,7 +1,5 @@
 from pydantic import BaseModel
 
-from app.shared.bible.domain.enum import AvailableBibleVersions
-
 
 class GetVersionsResponse(BaseModel):
-    versions: list[AvailableBibleVersions]
+    versions: dict[str, str]
