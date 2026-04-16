027f3b14bbda0a825880ca4391da740dcb2de27a
Minseok Oh <minsec9912@icloud.com>
2026-04-16 20:46:05 +0900

    feat: add exceptions for verse parsing

---
+++ api/app/bible/domain/exception/UnsupportedLetter.py
@@ -0,0 +1,2 @@
+class UnsupportedLetter(Exception):
+    pass
--- api/app/bible/domain/exception/__init__.py
+++ api/app/bible/domain/exception/__init__.py
@@ -1,11 +1,17 @@
 from .BookNotFound import BookNotFound
 from .ChapterNotFound import ChapterNotFound
+from .MultipleNumbers import MultipleNumbers
+from .MultipleSeparators import MultipleSeparators
 from .PhraseNotFound import PhraseNotFound
+from .UnsupportedLetter import UnsupportedLetter
 from .UnsupportedVersion import UnsupportedVersion
 
 __all__ = [
     "BookNotFound",
     "ChapterNotFound",
+    "MultipleNumbers",
+    "MultipleSeparators",
     "PhraseNotFound",
+    "UnsupportedLetter",
     "UnsupportedVersion",
 ]
