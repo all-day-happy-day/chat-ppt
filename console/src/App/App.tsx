6ebdfcffc42cc4f4074115464d1ecbcf248f73c3
Daniel Oh <minsec9912@icloud.com>
2026-04-23 06:34:21 +0900

    feat: add base providers and router, root layout

--- console/src/App/App.tsx
+++ console/src/App/App.tsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { QueryClientProvider, ThemeProvider } from '@/providers'
+
+import { GlobalErrorPage } from './layouts/misc/GlobalErrorPage/GlobalErrorPage'
+import { RootLayout } from './layouts/RootLayout/RootLayout'
+
 function App() {
-  return <div></div>
+  return (
+    <QueryClientProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<RootLayout />} />
+            <Route path="/error" element={<GlobalErrorPage />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
+  )
 }
 
 export default App
