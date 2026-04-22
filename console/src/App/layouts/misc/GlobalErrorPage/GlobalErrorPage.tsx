1479438c3626918eee3482b1ea77e9b6dccd1eff
Daniel Oh <minsec9912@icloud.com>
2026-04-23 06:33:17 +0900

    feat: add import i18n

--- console/src/App/layouts/misc/GlobalErrorPage/GlobalErrorPage.tsx
+++ console/src/App/layouts/misc/GlobalErrorPage/GlobalErrorPage.tsx
@@ -6,6 +6,8 @@ import { CenteredLayout } from '@/App/layouts/CenteredLayout/CenteredLayout'
 import { AnimatedOverlayButton } from '@/components/ui/AnimatedOverlayButton/AnimatedOverlayButton'
 import { useInterval } from '@/hooks/useInterval'
 
+import '@/i18n/i18n'
+
 const GLOBAL_TIMEOUT_RESET: number = 5
 
 export function GlobalErrorPage() {
@@ -27,7 +29,7 @@ export function GlobalErrorPage() {
       <div className="z-10 flex w-1/2 flex-col items-center justify-center gap-3">
         <ServerCrash className="text-brand-9 z-10" size="128" strokeWidth="1.5" />
         <p className="text-foreground z-10 text-center text-6xl font-bold wrap-break-word">
-          {t('globalErrorPage.title')}
+          {t('page.global_error.title')}
         </p>
         <p className="text-foreground/80 z-10 w-full text-center text-2xl font-semibold wrap-break-word">
           {t('page.global_error.description')}
@@ -52,7 +54,7 @@ const BackButton = () => {
     <AnimatedOverlayButton className="mt-8" onClick={onClickBackButton}>
       <div className="flex items-center gap-2 px-2 py-1 pr-3">
         <ArrowLeft size="32" strokeWidth="2.5" />
-        <span className="text-3xl font-semibold">{t('common.gloabl.back')}</span>
+        <span className="text-3xl font-semibold">{t('common.global.back')}</span>
       </div>
     </AnimatedOverlayButton>
   )
