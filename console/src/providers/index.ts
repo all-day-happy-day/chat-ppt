09e9965dd544543c03dd801734d3f1d4d6956fc5
Daniel Oh <minsec9912@icloud.com>
2026-04-25 07:07:48 +0900

    chore: export Theme

--- console/src/providers/index.ts
+++ console/src/providers/index.ts
@@ -1,3 +1,4 @@
 export { QueryClientProvider } from './QueryClientProvider'
+export type { Theme } from './theme-provider/ThemeProvider'
 export { ThemeProvider } from './theme-provider/ThemeProvider'
 export { useTheme } from './theme-provider/useTheme'
