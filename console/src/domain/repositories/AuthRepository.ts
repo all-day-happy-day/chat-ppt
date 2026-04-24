63925054b3414224d73b016d27c72d74f5cceca1
Daniel Oh <minsec9912@icloud.com>
2026-04-24 22:36:50 +0900

    feat: add auth repository abstract class

--- console/src/domain/repositories/AuthRepository.ts
+++ console/src/domain/repositories/AuthRepository.ts
@@ -1,10 +1,10 @@
-import type { Role, User } from '@/domain/models/User'
+import type { Role } from '@/domain/models/User'
 
 export abstract class AuthRepository {
-  abstract signIn(requestBody: { principal: string; password: string }): Promise<User>
+  abstract signIn(requestBody: { principal: string; password: string }): Promise<string>
   abstract signOut(): Promise<void>
-  abstract signUp(requestBody: { email: string; username: string; password: string; role?: Role }): Promise<User>
-  abstract verifyPassword(requestBody: { pasword: string }): Promise<void>
+  abstract signUp(requestBody: { email: string; username: string; password: string; role?: Role }): Promise<string>
+  abstract verifyPassword(requestBody: { principal: string; password: string }): Promise<void>
   abstract verify(): Promise<void>
   abstract reissue(): Promise<void>
 }
