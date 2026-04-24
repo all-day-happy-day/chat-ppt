75f530774eca78165b0d9240fa26c9f6ad668345
Daniel Oh <minsec9912@icloud.com>
2026-04-25 06:17:14 +0900

    feat: add default theme=system

--- console/src/App/App.tsx
+++ console/src/App/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { QueryClientProvider, ThemeProvider } from '@/providers'
 
+import { AuthenticatedLayout } from './layouts/authenticated-layout/AuthenticatedLayout'
 import { RootLayout } from './layouts/root-layout/RootLayout'
 import { GlobalErrorPage } from './pages/global-error-page/GlobalErrorPage'
 import { NotFoundPage } from './pages/not-found-page/NotFoundPage'
@@ -9,10 +10,11 @@ import { NotFoundPage } from './pages/not-found-page/NotFoundPage'
 export default function App() {
   return (
     <QueryClientProvider>
-      <ThemeProvider>
+      <ThemeProvider defaultTheme="system">
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<RootLayout />}>
+              <Route path="/" element={<AuthenticatedLayout />} />
               <Route path="/error" element={<GlobalErrorPage />} />
               <Route path="/notfound" element={<NotFoundPage />} />
             </Route>
