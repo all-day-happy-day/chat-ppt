5ea8446d5a65435ece21c610a1e55b87d19ca488
Daniel Oh <minsec9912@icloud.com>
2026-04-24 22:37:12 +0900

    feat: add auth message

---
+++ console/src/data/repositories/messages/RemoteAuthMessage.ts
@@ -0,0 +1,34 @@
+import type { Role } from '@/domain/models/User'
+
+interface BaseAuthResponse {
+  username: string
+}
+
+// SignIn
+export type SignInRequest = {
+  principal: string
+  password: string
+}
+export type SignInResponse = BaseAuthResponse
+
+// SignOut
+export type SignOutResponse = BaseAuthResponse
+
+// SignUp
+export type SignUpRequest = {
+  email: string
+  username: string
+  password: string
+  role?: Role
+}
+export type SignUpResponse = BaseAuthResponse
+
+// VerifyPassword
+export type VerifyPasswordRequest = {
+  principal: string
+  password: string
+}
+
+// Verify
+
+// Reissue
