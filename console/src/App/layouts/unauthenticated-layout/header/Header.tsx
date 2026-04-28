15866fdabed87efc376eda8d077a0f98f7d4f3ca
Daniel Oh <minsec9912@icloud.com>
2026-04-28 18:35:43 +0900

    fix: remove unnecessary get user

--- console/src/App/layouts/unauthenticated-layout/header/Header.tsx
+++ console/src/App/layouts/unauthenticated-layout/header/Header.tsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { useGetCurrentUser } from '@/api/query/auth.query'
 import { BaseHeader } from '@/components/common/header/BaseHeader'
-import type { User } from '@/domain/models/user'
-import { getQueryData } from '@/lib/utils'
 import { useTheme } from '@/providers'
 
 import { UnauthenticatedDropdownMenu } from './UnauthenticatedDropdownMenu'
@@ -12,10 +9,7 @@ import { UnauthenticatedDropdownMenu } from './UnauthenticatedDropdownMenu'
 export function Header() {
   const { t, i18n } = useTranslation()
   const { theme, setTheme } = useTheme()
-  const user = getQueryData<User>(useGetCurrentUser())
   const [language, setLanguage] = useState<string>(i18n.resolvedLanguage ?? i18n.language)
-
-  if (user === undefined) return null
 
   return (
     <BaseHeader>
