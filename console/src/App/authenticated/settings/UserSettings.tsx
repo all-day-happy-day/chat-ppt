066f649f717386b06cb40dfd83ed41abbb022c35
Daniel Oh <minsec9912@icloud.com>
2026-04-28 17:20:19 +0900

    feat: refactor usersettings page

--- console/src/App/authenticated/settings/UserSettings.tsx
+++ console/src/App/authenticated/settings/UserSettings.tsx
@@ -1,13 +1,18 @@
+import { Suspense, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 
-import { SaveButton } from '@/components/common/save-button/SaveButton'
+import { useGetCurrentUser } from '@/api/query/auth.query'
+import { usePatchUser } from '@/api/query/user.query'
+import { ActionButton } from '@/components/common/action-button/ActionButton'
 import { Card } from '@/components/ui/Card'
 import { InputField } from '@/components/ui/InputField'
-import type { Role } from '@/domain/models/user'
-import { useCurrentUser } from '@/hooks/useCurrentUser'
-import { cn } from '@/lib/utils'
+import type { Role, User } from '@/domain/models/user'
+import { cn, getQueryData } from '@/lib/utils'
 
 import '@/i18n/i18n'
+
+import type { UserSettingsForm } from './UserSettings.types'
 
 const ROLE_TEXT_CLASS: Record<Role, string> = {
   ADMIN: 'text-admin',
@@ -15,63 +20,89 @@ const ROLE_TEXT_CLASS: Record<Role, string> = {
 }
 
 export function UserSettings() {
-  const user = useCurrentUser().data ?? {
-    id: '1',
-    username: 'John Doe',
-    email: 'john.doe@example.com',
-    role: 'USER',
-    createdAt: new Date(),
-    lastSignIn: new Date(),
-  }
   const { t } = useTranslation()
+  const patchUser = usePatchUser()
+
+  const { register, handleSubmit, reset } = useForm<UserSettingsForm>({
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+    },
+  })
+  const user: User | undefined = getQueryData<User>(useGetCurrentUser())
+  useEffect(() => {
+    if (!user) return
+    reset({
+      username: user.username,
+      email: user.email,
+      password: '',
+    })
+  }, [user, reset])
+  const onSubmit = (data: UserSettingsForm) => {
+    if (!user) return
+    patchUser.mutate({
+      id: user.id,
+      requestBody: {
+        username: data.username,
+        email: data.email,
+        // password: data.password,
+      },
+    })
+  }
+
+  if (user === undefined) return null
 
   return (
-    <div className="flex flex-col items-center justify-center gap-16 pt-16">
-      <div className="flex w-1/3 flex-row justify-start border-b pb-4">
-        <p className="text-3xl font-semibold">{t('header.settings.settings')}</p>
+    <Suspense>
+      <div className="flex w-full flex-col items-center justify-center">
+        <div className="flex w-2xl flex-col gap-16 pt-16 max-md:w-fit">
+          <div className="flex flex-row justify-start border-b pb-4">
+            <p className="text-3xl font-semibold">{t('header.settings.settings')}</p>
+          </div>
+          <div className="flex items-center justify-between gap-16 px-1 max-md:flex-col max-md:justify-center md:flex-row">
+            <div className="flex max-w-full min-w-fit flex-col items-start justify-center gap-4 truncate">
+              <p className="text-5xl font-semibold">{user.username}</p>
+              <p className="text-muted-foreground pl-1 text-base">{user.email}</p>
+            </div>
+            <Card className="flex w-1/6 min-w-fit items-center justify-center p-4 text-xl font-bold capitalize shadow-[0_0_10px]">
+              <p className={cn(ROLE_TEXT_CLASS[user.role])}>{user.role}</p>
+            </Card>
+          </div>
+          <form
+            className="flex w-full flex-col items-start gap-16 max-md:items-center"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div className="flex flex-col items-start justify-between gap-6">
+              <InputField
+                register={register}
+                name="username"
+                label={t('common.user.username')}
+                className="w-xs max-md:w-full"
+              />
+              <InputField
+                register={register}
+                name="email"
+                label={t('common.user.email')}
+                className="w-xs max-md:w-full"
+              />
+            </div>
+            <div className="flex flex-col items-start gap-6">
+              <p className="text-destructive text-2xl font-semibold">{t('common.user.change_password')}</p>
+              <InputField
+                register={register}
+                name="password"
+                label={t('common.user.password')}
+                className="w-xs max-md:w-full"
+                required={false}
+              />
+            </div>
+            <div className="flex w-full flex-row items-center justify-end gap-2 max-md:justify-center">
+              <ActionButton type="submit" label={t('common.global.save')} />
+            </div>
+          </form>
+        </div>
       </div>
-      <div className="flex w-1/3 items-start justify-between gap-6 px-2 max-md:justify-center md:flex-row">
-        <div className="mb-6 flex max-w-full min-w-fit flex-col items-start gap-2 truncate">
-          <p className="text-5xl font-semibold">{user.username}</p>
-          <p className="text-muted-foreground p-1 text-base">{user.email}</p>
-        </div>
-        <Card className="flex w-1/5 min-w-fit items-center justify-center rounded-md text-xl font-bold capitalize">
-          <p className={cn(ROLE_TEXT_CLASS[user.role])}>{user.role}</p>
-        </Card>
-      </div>
-      <form
-        className="flex w-1/3 flex-col items-start gap-24"
-        onSubmit={(e: React.SubmitEvent<HTMLFormElement>) => {
-          e.preventDefault()
-          const form: HTMLFormElement = e.currentTarget
-          const data = new FormData(form)
-          const username = String(data.get('username') ?? '')
-          const email = String(data.get('email') ?? '')
-          console.log(username, email)
-        }}
-      >
-        <div className="flex flex-col items-start gap-6">
-          <InputField
-            id="username"
-            name="username"
-            label={t('common.user.username')}
-            type="text"
-            defaultValue={user.username}
-            className="max-md:w-full"
-          />
-          <InputField
-            id="email"
-            name="email"
-            label={t('common.user.email')}
-            type="email"
-            defaultValue={user.email}
-            className="max-md:w-full"
-          />
-        </div>
-        <div className="flex w-full flex-row items-center justify-end gap-2 max-md:justify-center">
-          <SaveButton type="submit" />
-        </div>
-      </form>
-    </div>
+    </Suspense>
   )
 }
