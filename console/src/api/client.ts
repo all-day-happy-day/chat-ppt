d2eb58c8719f0ea79731cd80f652d559015865a5
Daniel Oh <minsec9912@icloud.com>
2026-04-28 17:18:54 +0900

    chore: edit server address

--- console/src/api/client.ts
+++ console/src/api/client.ts
@@ -1,6 +1,6 @@
 import { APIClient } from './base-client/APIClient'
 
-export const httpClient = new APIClient(`http://${window.location.hostname}:10537`)
+export const httpClient = new APIClient(`http://${window.location.hostname}:8000`)
 
 httpClient.interceptors.request.use((request) => {
   console.log('[Request] Sent', request.url, request)
