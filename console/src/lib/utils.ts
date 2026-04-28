aa66ddf5abc2c90c075dcb4dd87b1c18d71d8412
Daniel Oh <minsec9912@icloud.com>
2026-04-28 17:25:33 +0900

    feat: add query and mutation data handling utils

--- console/src/lib/utils.ts
+++ console/src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+import type { DefaultError, UseMutationResult, UseQueryResult } from '@tanstack/react-query'
+
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs))
 }
@@ -12,8 +14,56 @@ export function getUserInitials(username: string): string {
     .join('')
 
   if (initial.length > 2) {
-    return initial.slice(0)
+    return initial.slice(0, 2)
   } else {
     return initial
   }
+}
+
+// Query / Mutation
+export type QueryDataState<TData, TError = DefaultError> =
+  | { kind: 'pending' }
+  | { kind: 'error'; error: TError }
+  | { kind: 'success'; data: TData }
+export type MutationDataState<TData, TError = DefaultError> =
+  | { kind: 'idle' }
+  | { kind: 'pending' }
+  | { kind: 'error'; error: TError }
+  | { kind: 'success'; data: TData }
+export function getQueryDataState<TData, TError = DefaultError>(
+  query: UseQueryResult<TData, TError>
+): QueryDataState<TData, TError> {
+  switch (query.status) {
+    case 'error':
+      return { kind: 'error', error: query.error }
+    case 'success':
+      return { kind: 'success', data: query.data }
+    case 'pending':
+      return { kind: 'pending' }
+  }
+}
+export function getMutationDataState<TData, TError = DefaultError>(
+  mutation: UseMutationResult<TData, TError>
+): MutationDataState<TData, TError> {
+  switch (mutation.status) {
+    case 'error':
+      return { kind: 'error', error: mutation.error }
+    case 'success':
+      return { kind: 'success', data: mutation.data }
+    case 'pending':
+      return { kind: 'pending' }
+    case 'idle':
+      return { kind: 'idle' }
+  }
+}
+
+export function getQueryData<TData, TError = DefaultError>(query: UseQueryResult<TData, TError>): TData | undefined {
+  const state: QueryDataState<TData, TError> = getQueryDataState(query)
+  return state.kind === 'success' ? state.data : undefined
+}
+export function getMutationData<TData, TError = DefaultError>(
+  mutation: UseMutationResult<TData, TError>
+): TData | undefined {
+  const state: MutationDataState<TData, TError> = getMutationDataState(mutation)
+  return state.kind === 'success' ? state.data : undefined
 }
