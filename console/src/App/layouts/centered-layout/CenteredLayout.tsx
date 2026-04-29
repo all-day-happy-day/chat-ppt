de980a231f028e956430f7672b99833ef8ecaee4
Daniel Oh <minsec9912@icloud.com>
2026-04-29 23:01:34 +0900

    chore: remove unnecessay divs

--- console/src/App/layouts/centered-layout/CenteredLayout.tsx
+++ console/src/App/layouts/centered-layout/CenteredLayout.tsx
@@ -1,13 +1,11 @@
-import { type PropsWithChildren } from 'react'
+import type { CSSProperties, PropsWithChildren } from 'react'
 
 export function CenteredLayout({ children }: PropsWithChildren) {
   return (
-    <main className="bg-background relative flex h-screen w-screen flex-col items-center justify-center gap-3 overflow-hidden">
-      <div
-        className="to-background/10 pointer-events-none absolute inset-0 z-0 bg-linear-to-tr from-transparent"
-        aria-hidden="true"
-      />
-      <div className="from-background absolute inset-0 z-0 bg-linear-to-t to-transparent" />
+    <main
+      className="bg-background relative flex h-screen w-screen flex-col items-center justify-center gap-3 overflow-hidden"
+      style={{ '--header-height': '48px' } as CSSProperties}
+    >
       {children}
     </main>
   )
