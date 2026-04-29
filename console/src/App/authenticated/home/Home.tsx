8b2056e2caafc26d55e5e98eb00eff87bfa9c067
Daniel Oh <minsec9912@icloud.com>
2026-04-29 23:02:21 +0900

    feat: add home page

---
+++ console/src/App/authenticated/home/Home.tsx
@@ -0,0 +1,31 @@
+import { Suspense } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import '@/i18n/i18n'
+
+import { ContentTable } from './home-card/content-table/ContentTable'
+import { HomeCard } from './home-card/HomeCard'
+
+export function Home() {
+  const { t } = useTranslation()
+
+  return (
+    <Suspense>
+      <div className="flex h-full w-full flex-col items-center justify-center">
+        <div className="flex h-full w-full flex-col items-center justify-center gap-16">
+          <div className="scrollbar-hide flex h-fit w-full max-w-[1200px] flex-row flex-wrap items-center justify-center gap-16 overflow-y-scroll p-16">
+            <HomeCard title={t('home.profile')}></HomeCard>
+            <HomeCard title={t('home.projects')}>
+              <ContentTable
+                contents={[<div>Project 1</div>, <div>Project 2</div>, <div>Project 3</div>]}
+                // icons={[<div>Icon1</div>, <div>Icon2</div>, <div>Icon3</div>]}
+              />
+            </HomeCard>
+            <HomeCard title={t('home.templates')}></HomeCard>
+            <HomeCard title={t('home.songs')}></HomeCard>
+          </div>
+        </div>
+      </div>
+    </Suspense>
+  )
+}
