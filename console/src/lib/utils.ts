ad8e9a68ec3d6251f408cb6927e837ee957f4566
Daniel Oh <minsec9912@icloud.com>
2026-05-01 00:42:56 +0900

    feat: add format date util

--- console/src/lib/utils.ts
+++ console/src/lib/utils.ts
@@ -67,3 +67,14 @@ export function getMutationData<TData, TError = DefaultError>(
   const state: MutationDataState<TData, TError> = getMutationDataState(mutation)
   return state.kind === 'success' ? state.data : undefined
 }
+
+// Date Formatting
+export function formatDate(date: Date): string {
+  const month: string = String(date.getMonth() + 1).padStart(2, '0')
+  const day: string = String(date.getDate()).padStart(2, '0')
+  const year: number = date.getFullYear()
+  const hours: string = String(date.getHours()).padStart(2, '0')
+  const minutes: string = String(date.getMinutes()).padStart(2, '0')
+  const seconds: string = String(date.getSeconds()).padStart(2, '0')
+  return `${month}/${day}/${year} ${hours}:${minutes}:${seconds}`
+}
