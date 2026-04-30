c985f6ebd287122225c2496a190559fdf7e20ed5
Daniel Oh <minsec9912@icloud.com>
2026-05-01 00:42:22 +0900

    feat: configure template table content

--- console/src/App/authenticated/home/home-card/content-table/template/TemplateTableContent.tsx
+++ console/src/App/authenticated/home/home-card/content-table/template/TemplateTableContent.tsx
@@ -1,19 +1,28 @@
 import { useGetCurrentUser } from '@/api/query/auth.query'
 import { useListTemplates } from '@/api/query/powerpoint.query'
-import type { Template, TemplateFile } from '@/domain/models/powerpoint'
+import { useGetUser } from '@/api/query/user.query'
+import type { TemplateResponse } from '@/domain/repositories/powerpoint-repository'
 import { getQueryData } from '@/lib/utils'
+import { formatDate } from '@/lib/utils'
 
 import type { ContentTableProps } from '../content-table-types'
 
-export function TemplateItem({ template, templateFile }: { template: Template; templateFile: TemplateFile }) {
+export function TemplateItem({ template }: { template: TemplateResponse | null }) {
+  const user = getQueryData(useGetUser(template?.userId ?? ''))
+  if (!user) return null
+
   return (
     <div className="flex h-full w-full flex-row items-center justify-between">
       <div className="flex flex-col items-start justify-center gap-1">
-        <div className="text-semibold text-md">{template.name}</div>
-        <div className="text-muted-foreground text-sm">{template.createdAt.toLocaleDateString()}</div>
+        {template && (
+          <>
+            <div className="text-semibold text-md">{template.name}</div>
+            <div className="text-muted-foreground text-sm">{user.username}</div>
+          </>
+        )}
       </div>
       <div className="flex flex-col items-end justify-center">
-        <div className="text-md">{templateFile.userId}</div>
+        {template && <div className="text-muted-foreground text-sm">{formatDate(new Date(template.createdAt))}</div>}
       </div>
     </div>
   )
@@ -22,28 +31,23 @@ export function TemplateItem({ template, templateFile }: { template: Template; t
 export function TemplateContentTableProps(): ContentTableProps {
   const currentUser = getQueryData(useGetCurrentUser())
   const listTemplates = useListTemplates(currentUser?.id ?? '')
+  let templates = getQueryData(listTemplates)
+
   if (!currentUser) return { contents: [null, null, null] }
+  if (!templates) return { contents: [null, null, null] }
 
-  const data = getQueryData(listTemplates)
-  if (!data || !data.templateFiles || !data.templates) return { contents: [null, null, null] }
-  let { templateFiles, templates } = data
-  console.log('template-check', templateFiles, templates)
-
-  if (templateFiles.length >= 3) {
-    templateFiles = templateFiles.slice(0, 3)
+  if (templates.length >= 3) {
     templates = templates.slice(0, 3)
   } else {
-    const emptyTemplateFiles: TemplateFile[] = Array(3 - templateFiles.length).fill(null)
-    const emptyTemplates: Template[] = Array(3 - templates.length).fill(null)
-    templateFiles = templateFiles.concat(emptyTemplateFiles)
+    const emptyTemplates: TemplateResponse[] = Array(3 - templates.length).fill(null)
     templates = templates.concat(emptyTemplates)
   }
 
   return {
     contents: [
-      <TemplateItem template={templates[0]} templateFile={templateFiles[0]} />,
-      <TemplateItem template={templates[1]} templateFile={templateFiles[1]} />,
-      <TemplateItem template={templates[2]} templateFile={templateFiles[2]} />,
+      <TemplateItem template={templates[0]} />,
+      <TemplateItem template={templates[1]} />,
+      <TemplateItem template={templates[2]} />,
     ],
     // icons: [<div>Icon1</div>, <div>Icon2</div>, <div>Icon3</div>],
   }
