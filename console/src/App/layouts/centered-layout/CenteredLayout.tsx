fc93ee94a46e1fa7d5c95798802778260977be57
Daniel Oh <minsec9912@icloud.com>
2026-04-30 21:34:08 +0900

    feat: remove overflow-hidden

--- console/src/App/layouts/centered-layout/CenteredLayout.tsx
+++ console/src/App/layouts/centered-layout/CenteredLayout.tsx
@@ -3,7 +3,7 @@ import type { CSSProperties, PropsWithChildren } from 'react'
 export function CenteredLayout({ children }: PropsWithChildren) {
   return (
     <main
-      className="bg-background relative flex h-screen w-screen flex-col items-center justify-center gap-3 overflow-hidden"
+      className="bg-background relative flex h-screen w-screen flex-col items-center justify-center gap-3"
       style={{ '--header-height': '48px' } as CSSProperties}
     >
       {children}
