021317b255ebbb5a261bb4e3405155c2b3f90327
Daniel Oh <minsec9912@icloud.com>
2026-04-30 11:29:27 +0900

    feat: add padding on the left

--- console/src/App/authenticated/home/home-card/HomeCard.tsx
+++ console/src/App/authenticated/home/home-card/HomeCard.tsx
@@ -7,7 +7,7 @@ export function HomeCard({ title, icon, children }: HomeCardProps & { children?:
     <Card className="h-[300px] w-[500px] rounded-xl transition-all duration-300 hover:scale-103">
       <CardHeader className="bg-accent/40 w-full rounded-t-xl p-4">
         {icon && icon}
-        <CardTitle>{title}</CardTitle>
+        <CardTitle className="pl-1">{title}</CardTitle>
       </CardHeader>
       <CardContent className="h-full w-full p-2">
         <Card className="h-full w-full shadow-none">{children && children}</Card>
