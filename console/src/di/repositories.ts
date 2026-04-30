279a42c0e0a6ac89b31520c510c82c5dfc23eac8
Daniel Oh <minsec9912@icloud.com>
2026-04-30 11:45:22 +0900

    feat: add song di

--- console/src/di/repositories.ts
+++ console/src/di/repositories.ts
@@ -1,7 +1,10 @@
 import { RemoteAuthRepository } from '@/data/repositories/remote-auth-repository'
+import { RemoteSongRepository } from '@/data/repositories/remote-song-repository'
 import { RemoteUserRepository } from '@/data/repositories/remote-user-repository'
 import type { AuthRepository } from '@/domain/repositories/auth-repository'
+import type { SongRepository } from '@/domain/repositories/song-repository'
 import type { UserRepository } from '@/domain/repositories/user-repository'
 
 export const authRepository: AuthRepository = new RemoteAuthRepository()
+export const songRepository: SongRepository = new RemoteSongRepository()
 export const userRepository: UserRepository = new RemoteUserRepository()
--- console/src/di/usecases.ts
+++ console/src/di/usecases.ts
@@ -1,7 +1,9 @@
 import { AuthUseCase } from '@/domain/usecases/auth-usecase'
+import { SongUseCase } from '@/domain/usecases/song-usecase'
 import { UserUseCase } from '@/domain/usecases/user-usecase'
 
-import { authRepository, userRepository } from './repositories'
+import { authRepository, songRepository, userRepository } from './repositories'
 
 export const authUseCase: AuthUseCase = new AuthUseCase(authRepository)
+export const songUseCase: SongUseCase = new SongUseCase(songRepository)
 export const userUseCase: UserUseCase = new UserUseCase(userRepository)
